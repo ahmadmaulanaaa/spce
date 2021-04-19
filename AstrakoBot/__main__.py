2e1fd91e30fd0144155f442cba38717672933aff
auzea <79923200+molana062@users.noreply.github.com>
2021-04-20 04:58:29 +0700

    Update __main__.py

--- AstrakoBot/__main__.py
+++ AstrakoBot/__main__.py
@@ -224,7 +224,6 @@ def start(update: Update, context: CallbackContext):
                 disable_web_page_preview=True,
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [
                             InlineKeyboardButton(
                                 text="Add Athaya",
                                 url="t.me/{}?startgroup=true".format(
@@ -246,8 +245,7 @@ def start(update: Update, context: CallbackContext):
                                  Text="Syntyche Official Group",
                                  url="https://t.me/syntychegroup",
                             )
-                        ],
-                    ]
+                      ]
                 ),
             )
     else:
